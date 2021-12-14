b246e69c1209f7e815c657c6467a116980b9f82f
GoldenCookieGIT <sashaheichenko@icloud.com>
2021-12-13 21:54:53 -0800

    add library for custom block data

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,16 @@ repositories {
     maven {
         url = uri("https://papermc.io/repo/repository/maven-public/")
     }
+    maven {
+        url = uri("https://hub.jeff-media.com/nexus/repository/jeff-media-public")
+    }
 }
 
 dependencies {
     compileOnly(kotlin("stdlib"))
     implementation("com.h2database:h2:2.0.202")
     implementation("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT")
+    implementation("de.jeff_media:CustomBlockData:1.0.3")
 
     compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
 
