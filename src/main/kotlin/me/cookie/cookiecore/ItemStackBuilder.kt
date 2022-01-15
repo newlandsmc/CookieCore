a30f5fb24a4af1078d8e0476c9b29a39347164d5
GoldenCookieGIT <sashaheichenko@icloud.com>
2022-01-14 21:12:29 -0800

    ItemFlag support

--- src/main/kotlin/me/cookie/cookiecore/ItemStackBuilder.kt
+++ src/main/kotlin/me/cookie/cookiecore/ItemStackBuilder.kt
@@ -2,35 +2,35 @@ package me.cookie.cookiecore
 
 import net.kyori.adventure.text.Component
 import org.bukkit.Material
+import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
 
 class ItemStackBuilder(private val type: Material) {
-    private var itemName: Component? = null
-    private var itemLore: List<Component>? = null
+    private var itemStack = ItemStack(type)
+    private var itemName = itemStack.displayName()
+    private var itemMeta = itemStack.itemMeta
+    private var itemLore = listOf<Component>()
+    private var itemFlags: Array<out ItemFlag>? = null
 
     fun withName(name: Component): ItemStackBuilder {
-        this.itemName = name
+        itemName = name
         return this
     }
 
     fun withLore(vararg lore: Component): ItemStackBuilder {
-        this.itemLore = lore.toList()
+        itemLore = lore.toList()
+        return this
+    }
+
+    fun withFlags(vararg flags: ItemFlag): ItemStackBuilder {
+        itemFlags = flags
         return this
     }
 
     fun build(): ItemStack {
-        val itemStack = ItemStack(type)
-        val itemMeta = itemStack.itemMeta
-
-        if(itemName == null){
-            itemMeta.displayName(Component.text(type.name))
-        }else{
-            itemMeta.displayName(itemName)
-        }
-
-        if(itemLore != null){
-            itemMeta.lore(itemLore)
-        }
+        itemMeta.displayName(itemName)
+        itemMeta.lore(itemLore)
+        itemFlags?.let { itemMeta.addItemFlags(*it) }
 
         itemStack.itemMeta = itemMeta
 
